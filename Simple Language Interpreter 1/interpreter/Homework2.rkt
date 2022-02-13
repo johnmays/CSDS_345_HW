8141e75361662f3ee58e7aa7c4eec1249117bbf9
Joshua Tang <tang.j1905@gmail.com>
2022-02-13 18:52:34 -0500

    basic state variable things

--- Simple Language Interpreter 1/interpreter/Homework2.rkt
+++ Simple Language Interpreter 1/interpreter/Homework2.rkt
@@ -9,18 +9,45 @@
 (define l_operand cadr)
 (define r_operand caddr)
 
+(define state_vars car)
+(define state_vals cadr)
+
+; ==================================================================================================
+;                                         HELPER FUNCTIONS
+; ==================================================================================================
+
+; Checks if an atom is a potential variable name
+(define var?
+  (lambda (x)
+    (not (or (pair? x) (null? x)))))
+
+; Retrieves the value of a given variable
+; Here, the state takes the form ((var1 var2 var3 ...) (val1 val2 val3 ...))
+(define M_varval
+  (lambda (var varlist vallist)
+    (cond
+      [(null? varlist) (error 'initerror "Variable not initialized")]
+      [(eq? var (car varlist)) (car vallist)]
+      [else (M_varval var (cdr varlist) (cdr vallist))])))
+
+
+; ==================================================================================================
+;                                       EVALUATION FUNCTIONS
+; ==================================================================================================
+
 ; Evaluates the value of an prefix mathematical expression
 (define M_value
-  (lambda (expr)
+  (lambda (expr state)
     (cond
-      [(number? expr) expr]
-      [(eq? (pre_op expr) '+) (+ (M_value (l_operand expr)) (M_value (r_operand expr)))]          ; Addition
+      [(number? expr) expr]                                                                                   ; Numeric
+      [(var? expr) (M_varval expr (state_vars state) (state_vals state))]                                     ; Variable
+      [(eq? (pre_op expr) '+) (+ (M_value (l_operand expr) state) (M_value (r_operand expr) state))]          ; Addition
       [(and (eq? (pre_op expr) '-) (not (null? (cddr expr))))
-       (- (M_value (l_operand expr)) (M_value (r_operand expr)))]                                 ; Subtraction
-      [(eq? (pre_op expr) '-) (- 0 (l_operand expr))]                                             ; Negation
-      [(eq? (pre_op expr) '*) (* (M_value (l_operand expr)) (M_value (r_operand expr)))]          ; Multiplication
-      [(eq? (pre_op expr) '/) (quotient (M_value (l_operand expr)) (M_value (r_operand expr)))]   ; Integer division
-      [(eq? (pre_op expr) '%) (remainder (M_value (l_operand expr)) (M_value (r_operand expr)))]  ; Modulus
+       (- (M_value (l_operand expr) state) (M_value (r_operand expr) state))]                                 ; Subtraction
+      [(eq? (pre_op expr) '-) (- 0 (l_operand expr) state)]                                                   ; Negation
+      [(eq? (pre_op expr) '*) (* (M_value (l_operand expr) state) (M_value (r_operand expr) state))]          ; Multiplication
+      [(eq? (pre_op expr) '/) (quotient (M_value (l_operand expr) state) (M_value (r_operand expr) state))]   ; Integer division
+      [(eq? (pre_op expr) '%) (remainder (M_value (l_operand expr) state) (M_value (r_operand expr) state))]  ; Modulus
       [else (error 'badop "Bad operator")])))
 
 ; Evaluates the result of a prefix boolean expression
@@ -32,4 +59,7 @@
      ; [(eq?
 
 
-; 
+; ==================================================================================================
+;                                         STATE FUNCTIONS
+; ==================================================================================================
+
